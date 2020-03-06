0b4c7a9fb251f2c2c730dad799146ce7c2ea13c0
Paula Cruz <47175612+paulacz@users.noreply.github.com>
2020-03-05 22:24:51 -0300

    Rename L1 - N5.cpp to aulasprimeiroperiodo/L1 - N5.cpp

--- aulasprimeiroperiodo/L1 - N5.cpp
+++ aulasprimeiroperiodo/L1 - N5.cpp
@@ -1,15 +1,15 @@
 /* IPOG - Engenharia Civil
    Paula Cruz - 08/2019
    Estrutura Sequencial - N5
-   Faça um algoritmo que leia três numeros. Calcule
-   e escreva a média aritmética.
+   FaÃ§a um algoritmo que leia trÃªs numeros. Calcule
+   e escreva a mÃ©dia aritmÃ©tica.
 */
 
 #include <stdio.h>
 
 int main() {
 	
-	//declaração de variáveis
+	//declaraÃ§Ã£o de variÃ¡veis
 	int num1, num2, num3, media;
 	
 	//entrada de dados
